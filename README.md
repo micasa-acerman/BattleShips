01fc1b2781e8885de4fc85145aae55a3f5b18d02
Konstantin M <mrikaevkm@dev002.local>
2021-09-19 17:35:29 +0300

    docs: describe game process

--- README.md
+++ README.md
@@ -1,46 +1,37 @@
-# Getting Started with Create React App
+# Морской бой 
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Инструкция по запуску
 
-## Available Scripts
+В корневой директории необходимо выполнить:
 
-In the project directory, you can run:
+`yarn start`
 
-### `yarn start`
+Эта команда запускает сайт в режиме разработчика.\
+Откройте [http://localhost:3000](http://localhost:3000) для просмотра в браузере.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Страничка автоматически обновиться после изменения кодовой базы.\
+Так же, в случае возникновения ошибки, в консоле вы увидите сводку.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+Если вы хотите собрать продуктивную сборку, то выполните:  
 
-### `yarn test`
+`yarn build`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Собранный бандл вы сможете найти в папке `build`.
 
-### `yarn build`
+Продуктивная версия будет минифицированна и в бандлах будут использоваться хеши.
+Далее вы можете разместить разместить на подготовленной среде либо в [docker](https://hub.docker.com/_/httpd) контейнере.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Взаимосвязь компонентов
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+![file structure](doc/file-structure.png)
+Скрипт | Описание
+------------ | -------------
+App.tsx | Определяет структуру сайта (header, body, footer)
+GameMananger.tsx | Содержит в себе состояния кораблей, так же содержит в себе основную игровую логику.
+LabelsFields.tsx | Является префабом. Используется для отрисовки горизонтальный и вертикальных отметок осей.
+BattleField.tsx | Отрисовывает основные игровые элементы. Так же передает основные игровые события (клик по неотмеченной клетке и т.п.)
+shipHelper.tsx | Содержит вспомогательные функции используемые для описания объектов BattleField.
+fieldHelper.tsx | Содержит функции реализующие основные методы для для работы с BattleField (К примеру, проверка позиции, результат игры, расположением элементов при d&d)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Алгоритм
+![alghorithm](doc/alghorithm.png)
--- src/components/utils/fieldHelper.test.ts
+++ src/components/utils/fieldHelper.test.ts
@@ -7,158 +7,195 @@ import {
   isSquareLocatedInArea,
   transformPositionFromCellsToPx,
 } from "./fieldHelper";
+describe("fieldHelper", () => {
+  test("check function transformPositionFromCellsToPx", async () => {
+    expect(transformPositionFromCellsToPx(10)).toBe(250);
+    expect(transformPositionFromCellsToPx(0)).toBe(0);
+    expect(transformPositionFromCellsToPx(1)).toBe(25);
+  });
 
-test("check function transformPositionFromCellsToPx", async () => {
-  expect(transformPositionFromCellsToPx(10)).toBe(250);
-  expect(transformPositionFromCellsToPx(0)).toBe(0);
-  expect(transformPositionFromCellsToPx(1)).toBe(25);
-});
+  test("check function isPointLocatedInArea", async () => {
+    expect(
+      isPointLocatedInArea(
+        {
+          position: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            width: 10,
+            height: 10,
+          },
+        },
+        {
+          x: 1,
+          y: 1,
+        }
+      )
+    ).toBeTruthy();
+    expect(
+      isPointLocatedInArea(
+        {
+          position: {
+            x: 1,
+            y: 1,
+          },
+          size: {
+            width: 10,
+            height: 10,
+          },
+        },
+        {
+          x: 1,
+          y: 1,
+        }
+      )
+    ).toBeTruthy();
+    expect(
+      isPointLocatedInArea(
+        {
+          position: {
+            x: 1,
+            y: 1,
+          },
+          size: {
+            width: 10,
+            height: 10,
+          },
+        },
+        {
+          x: -1,
+          y: 1,
+        }
+      )
+    ).toBeFalsy();
+  });
 
-test("check function isPointLocatedInArea", async () => {
-  expect(
-    isPointLocatedInArea(
-      {
-        position: {
-          x: 0,
-          y: 0,
+  test("check function isSquareLocatedInArea", async () => {
+    expect(
+      isSquareLocatedInArea(
+        {
+          position: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            width: 10,
+            height: 10,
+          },
         },
-        size: {
-          width: 10,
-          height: 10,
+        {
+          position: {
+            x: 0,
+            y: 9,
+          },
+          size: {
+            width: 1,
+            height: 1,
+          },
+        }
+      )
+    ).toBeTruthy();
+    expect(
+      isSquareLocatedInArea(
+        {
+          position: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            width: 5,
+            height: 5,
+          },
         },
-      },
-      {
+        {
+          position: {
+            x: 4,
+            y: 4,
+          },
+          size: {
+            width: 5,
+            height: 1,
+          },
+        }
+      )
+    ).toBeFalsy();
+  });
+  test("check function isCrossAreas", async () => {
+    expect(
+      isCrossAreas(
+        {
+          position: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            width: 5,
+            height: 5,
+          },
+        },
+        {
+          position: {
+            x: 4,
+            y: 4,
+          },
+          size: {
+            width: 5,
+            height: 5,
+          },
+        }
+      )
+    ).toBeTruthy();
+    expect(
+      isCrossAreas(
+        {
+          position: {
+            x: 1,
+            y: 1,
+          },
+          size: {
+            width: 6,
+            height: 3,
+          },
+        },
+        {
+          position: {
+            x: 2,
+            y: 1,
+          },
+          size: {
+            width: 4,
+            height: 4,
+          },
+        }
+      )
+    ).toBeTruthy();
+  });
+  test("check function isAvailableElementPosition", async () => {
+    const area = {
+      position: {
         x: 1,
         y: 1,
-      }
-    )
-  ).toBeTruthy();
-  expect(
-    isPointLocatedInArea(
+      },
+      size: {
+        width: 10,
+        height: 10,
+      },
+    };
+    const forbidden = [
       {
         position: {
           x: 1,
           y: 1,
         },
         size: {
-          width: 10,
-          height: 10,
-        },
-      },
-      {
-        x: 1,
-        y: 1,
-      }
-    )
-  ).toBeTruthy();
-  expect(
-    isPointLocatedInArea(
-      {
-        position: {
-          x: 1,
-          y: 1,
-        },
-        size: {
-          width: 10,
-          height: 10,
-        },
-      },
-      {
-        x: -1,
-        y: 1,
-      }
-    )
-  ).toBeFalsy();
-});
-
-test("check function isSquareLocatedInArea", async () => {
-  expect(
-    isSquareLocatedInArea(
-      {
-        position: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 10,
-          height: 10,
-        },
-      },
-      {
-        position: {
-          x: 0,
-          y: 9,
-        },
-        size: {
-          width: 1,
-          height: 1,
-        },
-      }
-    )
-  ).toBeTruthy();
-  expect(
-    isSquareLocatedInArea(
-      {
-        position: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 5,
-          height: 5,
-        },
-      },
-      {
-        position: {
-          x: 4,
-          y: 4,
-        },
-        size: {
-          width: 5,
-          height: 1,
-        },
-      }
-    )
-  ).toBeFalsy();
-});
-test("check function isCrossAreas", async () => {
-  expect(
-    isCrossAreas(
-      {
-        position: {
-          x: 0,
-          y: 0,
-        },
-        size: {
-          width: 5,
-          height: 5,
-        },
-      },
-      {
-        position: {
-          x: 4,
-          y: 4,
-        },
-        size: {
-          width: 5,
-          height: 5,
-        },
-      }
-    )
-  ).toBeTruthy();
-  expect(
-    isCrossAreas(
-      {
-        position: {
-          x: 1,
-          y: 1,
-        },
-        size: {
-          width: 6,
           height: 3,
+          width: 6,
         },
       },
-      {
+    ];
+    expect(
+      isAvailableElementPosition(area, forbidden, {
         position: {
           x: 2,
           y: 1,
@@ -167,83 +204,47 @@ test("check function isCrossAreas", async () => {
           width: 4,
           height: 4,
         },
-      }
-    )
-  ).toBeTruthy();
-});
-test("check function isAvailableElementPosition", async () => {
-  const area = {
-    position: {
-      x: 1,
-      y: 1,
-    },
-    size: {
-      width: 10,
-      height: 10,
-    },
-  };
-  const forbidden = [
-    {
+      })
+    ).toBeFalsy();
+  });
+
+  test("check function getAvailableElementPosition", async () => {
+    const area = {
       position: {
         x: 1,
         y: 1,
       },
       size: {
-        height: 3,
-        width: 6,
+        width: 11,
+        height: 11,
       },
-    },
-  ];
-  expect(
-    isAvailableElementPosition(area, forbidden, {
-      position: {
-        x: 2,
-        y: 1,
+    };
+    const forbidden = [
+      {
+        position: {
+          x: 2,
+          y: 2,
+        },
+        size: {
+          height: 1,
+          width: 4,
+        },
       },
-      size: {
-        width: 4,
-        height: 4,
-      },
-    })
-  ).toBeFalsy();
-});
-
-test("check function getAvailableElementPosition", async () => {
-  const area = {
-    position: {
-      x: 1,
-      y: 1,
-    },
-    size: {
-      width: 11,
-      height: 11,
-    },
-  };
-  const forbidden = [
-    {
-      position: {
-        x: 2,
-        y: 2,
-      },
-      size: {
-        height: 1,
-        width: 4,
-      },
-    },
-  ];
-  expect(
-    getAvailableElementPosition(area, forbidden, {
-      position: {
-        x: 2,
-        y: 7,
-      },
-      size: {
-        width: 1,
-        height: 3,
-      },
-    })
-  ).toEqual({
-    x: 2,
-    y: 7,
+    ];
+    expect(
+      getAvailableElementPosition(area, forbidden, {
+        position: {
+          x: 2,
+          y: 7,
+        },
+        size: {
+          width: 1,
+          height: 3,
+        },
+      })
+    ).toEqual({
+      x: 2,
+      y: 7,
+    });
   });
 });
